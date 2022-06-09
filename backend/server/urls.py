b9c2264414053c95479bcd2f433d09784ff23178
OlegSMI <44391839+OlegSMI@users.noreply.github.com>
2022-06-10 00:42:38 +0300

    home

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('users', views.UserListView.as_view()),
     path('users/<int:num_user>', views.MilitaryOneUser.as_view()),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('data', views.GetUserInfoInSosialNetwork.as_view())
 ]
--- backend/server/views.py
+++ backend/server/views.py
@@ -1,5 +1,8 @@
+from rest_framework.response import Response
+
 # from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, CreateAPIView
+from rest_framework.views import APIView
 from .models import InfoUser
 from .serializers import InfoUserSerializer
 
@@ -12,5 +15,19 @@ class MilitaryOneUser(RetrieveAPIView):
     serializer_class = InfoUserSerializer
     lookup_field = 'num_user'
     
-class CommentsView(CreateAPIView):
-    queryset = request.get('http://127.0.0.1:8888/groups/comments/')
+
+class GetUserInfoInSosialNetwork(APIView):
+
+    def get(self, request):
+        posts = request.get(f'http://127.0.0.1:8888/user/posts/?user={id}')
+        comments_1 = request.get(f'http://127.0.0.1:8888/user/posts/comments/?owner_comment_text={id}')
+        comments_2 = request.get(f'http://127.0.0.1:8888/groups/comments/user/?owner_comment_text={id}')
+        groups = request.get(f'http://127.0.0.1:8888/groups/user/?group_user={id}')
+
+        # friends and info about this user
+        infouser = request.get(f'http://127.0.0.1:8888/user/get_info/{id}')
+        print(posts)
+
+        return Response({"posts": posts})
+
+
