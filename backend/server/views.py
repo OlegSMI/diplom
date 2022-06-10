9bdd80e241276a1ceddd7f915d35b127c1df03fb
OlegSMI <slovplanmail.ru>
2022-06-10 13:45:44 +0300

    totalchange

--- backend/server/views.py
+++ backend/server/views.py
@@ -1,4 +1,6 @@
+from traceback import print_list
 from rest_framework.response import Response
+import requests
 
 # from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, CreateAPIView
@@ -18,16 +20,23 @@ class MilitaryOneUser(RetrieveAPIView):
 
 class GetUserInfoInSosialNetwork(APIView):
 
+    def get(self, request, id=1):
+        posts = requests.get(f'http://127.0.0.1:8888/user/posts/?user={id}')
+        comments_1 = requests.get(f'http://127.0.0.1:8888/user/posts/comments/?owner_comment_text={id}')
+        comments_2 = requests.get(f'http://127.0.0.1:8888/groups/comments/user/?owner_comment_text={id}')
+        groups = requests.get(f'http://127.0.0.1:8888/groups/user/?group_user={id}')
+        infouser = requests.get(f'http://127.0.0.1:8888/user/get_info/{id}')
+        
+        return Response({
+            "infouser": infouser.json(),
+            "posts": posts.json(),
+            "groups": groups.json(),
+            "comments": comments_1.json()+comments_2.json(),
+        })
+
+
+class GetAllUserInfoInSosialNetwork(APIView):
+    
     def get(self, request):
-        posts = request.get(f'http://127.0.0.1:8888/user/posts/?user={id}')
-        comments_1 = request.get(f'http://127.0.0.1:8888/user/posts/comments/?owner_comment_text={id}')
-        comments_2 = request.get(f'http://127.0.0.1:8888/groups/comments/user/?owner_comment_text={id}')
-        groups = request.get(f'http://127.0.0.1:8888/groups/user/?group_user={id}')
 
-        # friends and info about this user
-        infouser = request.get(f'http://127.0.0.1:8888/user/get_info/{id}')
-        print(posts)
-
-        return Response({"posts": posts})
-
-
+        
